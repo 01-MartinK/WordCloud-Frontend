2912f1b40bbc8debc98cdbd3743b9009e1867a21
MK <46935118+TheDeadGamer101@users.noreply.github.com>
2023-11-14 14:30:47 +0200

    File handling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import FileDropzone from './components/FileDropzone';
+
+import { FormControl, FormLabel, Input, Button, Textarea } from '@mui/joy';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main'>
+        <FormControl className="access" >
+          <FormLabel>Access Code</FormLabel>
+          <Input type="text" placeholder='Access Code' />
+          <Button>Get Data</Button>
+        </FormControl>
+        <div className='deliminer' />
+        <FormLabel>Upload File</FormLabel>
+        <FileDropzone />
+        <FormLabel>Write a sentence</FormLabel>
+        <textarea className="textarea"></textarea>
+        <br />
+        <Button>Analyze Text</Button>
+      </div>
+      <div className='output'>
+
+      </div>
     </div>
   );
 }
